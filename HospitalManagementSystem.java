70cbee7df66455ab3f8106ad889eee49a7765b13
yaradesiBhavyaSri <yaradesibhavyasri05@gmail.com>
2025-03-26 21:39:59 +0530

    Update HospitalManagementSystem.java

--- HospitalManagementSystem.java
+++ HospitalManagementSystem.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class HospitalManagementSystem {
     private static final String url = "jdbc:mysql://localhost:3306/hospital";
     private static final String username = "root";
-    private static final String password = "SubhadraRavi@20";
+    private static final String password = "admini@20";
 
     public static void main(String[] args) {
         try{
